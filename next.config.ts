5b4d2ac622615e9119bfcba3dbd45c86b41ea86b
AddYQnm <163165302+AddYQnm@users.noreply.github.com>
2026-02-25 02:26:31 +0100

    03

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: { unoptimized: true },
+  // ❌ retire output export
+  // output: "export",
+  images: { unoptimized: true }, // tu peux garder si tu veux
 };
 
 export default nextConfig;
